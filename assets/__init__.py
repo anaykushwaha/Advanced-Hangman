14c725b45261e1078e0ac7b1cc3f6a3cbf8e2ff2
Anay Kushwaha <anaykushwaha@gmail.com>
2026-07-20 14:23:25 +0530

    Finalizing assets directory

---
+++ assets/__init__.py
@@ -0,0 +1,13 @@
+""" 
+Advanced Hangman 
+Assets Package 
+
+This package contains all visual resources used throughout the 
+Advanced Hangman project. It provides terminal styling, ASCII art, 
+and the graphical Hangman frames that are rendered during gameplay 
+
+Files: 
+- logo.txt: ASCII art title displayed at startup 
+- colors.py: ANSI color codes and text formatting 
+- hangman_frames.py: Progressive Hangman scaffold and body drawings 
+""" 
