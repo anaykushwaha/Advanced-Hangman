002306299f7eea0f293e0289d2204aa9e05a969e
Anay Kushwaha <anaykushwaha@gmail.com>
2026-07-20 14:24:09 +0530

    Finalizing utils directory

---
+++ utils/__init__.py
@@ -0,0 +1,16 @@
+""" 
+Advanced Hangman 
+Utilities Package 
+
+This package contains helper modules shared across the entire 
+Advanced Hangman project. These modules provide reusable utilities, 
+constants, animations, file management, banners, and miscellaneous 
+helper functions 
+
+Files: 
+- helper.py: General-purpose helper functions 
+- constants.py: Global constants used throughout the project 
+- animations.py: Terminal animation utilities 
+- banner.py: Startup and menu banner rendering 
+- file_manager.py: File and directory management utilities 
+""" 
