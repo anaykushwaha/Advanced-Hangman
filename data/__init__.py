efd58673c68da0f0ad0fc8c90f32e1a244be7f90
Anay Kushwaha <anaykushwaha@gmail.com>
2026-07-21 22:34:44 +0530

    Finalizing data directory

---
+++ data/__init__.py
@@ -0,0 +1,16 @@
+""" 
+Advanced Hangman - Data Package 
+This directory stores all persistent data used by the game 
+
+Contents: 
+- easy_words.json 
+- medium_words.json 
+- hard_words.json 
+- impossible_words.json 
+- statistics.json 
+- saves/savegame.json 
+
+These JSON files provide the game's word databases, player 
+statistics, and save-game information. They are loaded and 
+updated automatically by the game's data management systems 
+""" 
